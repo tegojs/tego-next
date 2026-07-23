8ac705cdbfe9c4b15634afe3a14fa336a5e581cb
张琳 Lin Zhang <sealday@gmail.com>
2026-07-23 16:12:13 +0800

    refactor: keep SQLite migrations private

--- packages/drivers-local/src/index.ts
+++ packages/drivers-local/src/index.ts
@@ -2,5 +2,4 @@ export * from "./create-local-drivers.js";
 export * from "./filesystem-artifact-store.js";
 export * from "./local-coordination.js";
 export * from "./memory-state-store.js";
-export * from "./sqlite/migrations.js";
 export * from "./sqlite/sqlite-state-store.js";
